19d5ba82be7811e0f789869fa7a609b48529a3f8
jalex2210 <alexbenitez.rdm@gmail.com>
2023-09-30 18:17:07 -0500

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const selectorGallery = document.querySelector("ul.gallery");
+selectorGallery.classList.add("gallery");
+
+galleryItems.forEach(function (galleryItem) {
+  const makerLi = document.createElement("li");
+  makerLi.classList.add("gallery__item");
+  selectorGallery.appendChild(makerLi);
+
+  const makerBtn = document.createElement("a");
+  makerLi.append(makerBtn);
+  makerBtn.classList.add("gallery__link");
+  makerBtn.href = galleryItem.original;
+
+  const makerImg = document.createElement("img");
+  makerBtn.append(makerImg);
+  makerImg.classList.add("gallery__image");
+  makerImg.src = galleryItem.preview;
+  makerImg.dataset.source = galleryItem.original;
+  makerImg.alt = galleryItem.description;
+
+  makerBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+let gallery = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
